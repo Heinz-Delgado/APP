c114218ff519759e4efeb31808462bdd0fd3ef60
Heinz <hddelgadoo@utn.edu.ec>
2022-08-15 16:34:53 -0500

    Proyecto creado

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 import { HomePage } from './home.page';
 
@@ -21,4 +23,30 @@ describe('HomePage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  interface MenuCustomEvent<T = any> extends CustomEvent {
+    detail: T;
+    target: HTMLIonMenuElement;
+  }
+  @Component({
+    selector: 'menu-example',
+    templateUrl: 'menu-example.html',
+    styleUrls: ['./menu-example.css'],
+  })
+  export class MenuExample {
+  
+  constructor(private menu: MenuController) { }
+  
+    openFirst() {
+      this.menu.enable(true, 'first');
+      this.menu.open('first');
+    }
+  
+    openEnd() {
+      this.menu.open('end');
+    }
+  
+    openCustom() {
+      this.menu.enable(true, 'custom');
+      this.menu.open('custom');
+    }
 });
